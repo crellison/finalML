56729ca7b266504ac3c283ac7a2df86e924c49ce
Cole Ellison <cole.ellison@gmail.com>
2017-11-29 16:41:01 -0500

    grab all of the pairs

--- wrangledata.py
+++ wrangledata.py
@@ -1,5 +1,6 @@
 from csv import reader
 from random import shuffle, choice, choices, sample
+from sys import stdout
 
 DATA_DIR = 'data/'
 TRAIN_DATA = DATA_DIR + 'train/'
@@ -50,31 +51,46 @@ def create_pairs():
       else:
         data[artistID].append(filepath)
 
-  artists = list(data)
   artist_weights = [len(data[ID]) for ID in data]
-  print('%i differnt artists with %i total paintings' % (len(artists), m))
+  artists = list(data)
+  n = len(artists)
+  print('%i differnt artists with %i total paintings' % (n, m))
   pairs = []
+  prog = 0
+  proglen = 78
   for artistID in artists:
-    num_paintings = len(data[artistID])
-    num_pairs = min(MAX_ARTIST_PAIRS, num_paintings // 2)
+    prog += 1
+    progress = int(prog/n * proglen)
+    bar = '=' * progress
+    empty = ' ' * (proglen-progress)
+    stdout.write('\r[%s%s]' % (bar, empty))
 
-    # collect num_pairs pairs of paintings from the same artist
+    num_pairs = sum(x for x in range(len(data[artistID])))
+
+    # collect all possible pairs of paintings from the same artist
     # tuples of form (path, path, y) with y=1 if same artist else y=0
     painting_pairs = all_pairs(data[artistID])
     same_pairs = [(pair[0], pair[1], '1') for pair in painting_pairs]
     pairs.extend(same_pairs)
 
-    # collect 1 training example per painting with different artists
+    # collect factorial(num_pairs) training example per painting with different artists
+    # should have same number diff artist pairs as same artist pairs
     diff_pairs = []
-    for painting in data[artistID]:
+    
+    for i in range(num_pairs):
+      cur_painting = choice(data[artistID])
+
       rand_artist = choices(artists, weights=artist_weights)[0]
       while rand_artist == artistID:
         rand_artist = choices(artists, weights=artist_weights)[0]
+
       rand_painting = choice(data[rand_artist])
-      diff_pairs.append((painting, rand_painting, '0'))
+      diff_pairs.append((cur_painting, rand_painting, '0'))
 
     pairs.extend(diff_pairs)
 
+  stdout.flush()
+  print()
   return pairs
 
 def all_pairs(item_list):
@@ -85,10 +101,11 @@ def all_pairs(item_list):
 
 def main():
   pairs = create_pairs()
+  print('pairs generated, shuffling pairs')
   shuffle(pairs)
   m = len(pairs)
   pos = sum(int(pair[2]) for pair in pairs)
-  print('%i training points with %i positive examples' % (m, pos))
+  print('%i training points with %1.4f percent positive examples' % (m, pos/m*100))
   with open(PAIR_TRAIN_SET, 'w+') as writer:
     for pair in pairs:
       writer.write(','.join(pair)+'\n')
