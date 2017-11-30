6f6244ec030babb0cb1b64a1e5fa4bf214354321
Cole Ellison <cole.ellison@gmail.com>
2017-11-30 11:08:46 -0500

    make get_data_info importable

--- get_data_info.py
+++ get_data_info.py
@@ -2,29 +2,36 @@ from PIL import Image
 import os
 import numpy
 
-image_path = os.path.join('data', 'train')
+IMAGE_PATH = os.path.join('data', 'train')
 
-widths = []
-heights = []
+def get_median_size():
+    widths = []
+    heights = []
 
-print(os.path.abspath(image_path))
+    print(os.path.abspath(IMAGE_PATH))
 
-for image_file in os.listdir(image_path):
-    with Image.open(os.path.join(image_path, image_file)) as image:
-        widths.append(image.width)
-        heights.append(image.width)
+    for image_file in os.listdir(IMAGE_PATH):
+        with Image.open(os.path.join(IMAGE_PATH, image_file)) as image:
+            widths.append(image.width)
+            heights.append(image.width)
 
-widths.sort()
-heights.sort()
+    widths.sort()
+    heights.sort()
 
-median_size = (widths[len(widths) // 2], heights[len(heights) // 2])
+    median_size = (widths[len(widths) // 2], heights[len(heights) // 2])
+    return widths, heights, median_size
 
-print('Median image size: ', median_size)
-# OUTPUTS (800, 800)
+def main():
+    widths, heights, median_size = get_median_size()
+    print('Median image size: ', median_size)
+    # OUTPUTS (800, 800)
+    
+    # width_histogram = numpy.histogram(widths)
+    # height_histogram = numpy.histogram(heights)
+    # print(width_histogram)
+    # print(height_histogram)
+    
+if __name__ == '__main__':
+    main()
 
-"""
-width_histogram = numpy.histogram(widths)
-height_histogram = numpy.histogram(heights)
-print(width_histogram)
-print(height_histogram)
-"""
+
