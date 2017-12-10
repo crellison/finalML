c568418ab75c754d8ea40fb497b8d16e2edca101
HansGoudey <33431250+HansGoudey@users.noreply.github.com>
2017-12-09 21:08:21 -0500

    Changed to epochs vs batches

--- siamese.py
+++ siamese.py
@@ -11,12 +11,12 @@ import sys
 import siamese_utils
 from siamese_utils import *
 
-IMAGE_DIM = 100
+IMAGE_DIM = 200
 
-TRAIN_SPLIT = 0.6
-CV_TEST_SPLIT = 0.8
+TRAIN_SPLIT = 0.8
+CV_TEST_SPLIT = 0.9
 
-BATCH_SIZE = 32
+BATCH_SIZE = 8
 
 DATA_DIR = os.path.join('data', 'train')
 OUTPUT_FILE = os.path.join('Models', 'painting_model')
@@ -25,7 +25,7 @@ OUTPUT_FILE = os.path.join('Models', 'painting_model')
 def create_model(input_shape, sub_net_choice):
     """
     Returns a siamese model with a subnet work chosen with sub_net_choice:
-        'large': cnn with 4 convolution layers
+    'large': cnn with 4 convolution layers
     """
     twin_net = {'large': create_large_sub_net}[sub_net_choice](input_shape)
 
@@ -50,7 +50,6 @@ def create_model(input_shape, sub_net_choice):
 
 
 def create_large_sub_net(input_shape):
-    print('creating subnetwork...')
     model = Sequential()
 
     model.add(Conv2D(32, (9, 9), padding='same', activation='relu', input_shape=input_shape))
@@ -59,22 +58,17 @@ def create_large_sub_net(input_shape):
     model.add(Conv2D(64, (7, 7), padding='same', activation='relu'))
     model.add(MaxPooling2D())
 
-    model.add(Conv2D(64, (5, 5), padding='same', activation='relu'))
+    model.add(Conv2D(128, (5, 5), padding='same', activation='relu'))
     model.add(MaxPooling2D())
 
-    # model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
-
     model.add(Flatten())
-
     model.add(Dense(1024, activation='relu'))
-    model.add(Dense(1024, activation='relu'))
+    model.add(Dense(512, activation='relu'))
 
     model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
-
-
-def train_model(model, image_size, n_train_batches, pairs_csv):
+def train_model(model, image_size, epochs, pairs_csv):
 
     # Open the pre-made file of pairs
     with open(pairs_csv) as pairs_file:
@@ -90,28 +84,47 @@ def train_model(model, image_size, n_train_batches, pairs_csv):
     test_pairs = pairs[cv_test_split:]
 
     # Train the model
-    for batch in range(n_train_batches):
-        print('Training batch', batch)
-        train_batch_a, train_batch_b, train_batch_y = get_batch(train_pairs, (batch * BATCH_SIZE) % len(pairs), image_size)
+    batches_per_epoch = len(train_pairs) // BATCH_SIZE
+    print('Total batches to train', batches_per_epoch * epochs)
+    print('train batches', len(train_pairs) // BATCH_SIZE)
+    fractional_epoch = epochs - np.floor(epochs)
+    epochs = int(np.floor(epochs))
 
+    # Train full epochs
+    for epoch in range(epochs):
+        print('Training epoch', epoch)
+        for batch in range(batches_per_epoch):
+            print('Training batch', batches_per_epoch * epoch + batch)
+            train_batch_a, train_batch_b, train_batch_y = get_batch(train_pairs, (batch * BATCH_SIZE), image_size)
+            model.train_on_batch([train_batch_a, train_batch_b], train_batch_y)
+
+    # Train remaining batches
+    print('fractional epoch', fractional_epoch)
+    print('batches to train', int(batches_per_epoch * fractional_epoch))
+    for batch in range(int(batches_per_epoch * fractional_epoch)):
+        print('Training batch', batches_per_epoch * epochs + batch)
+        train_batch_a, train_batch_b, train_batch_y = get_batch(train_pairs, (batch * BATCH_SIZE), image_size)
         model.train_on_batch([train_batch_a, train_batch_b], train_batch_y)
 
+    # Save the model for
+    print('Training finished. Saving model.')
     model.save(outfile(OUTPUT_FILE))
 
-    # Test image batches
-    all_predictions = np.ndarray((1, 1))
-    all_y = np.ndarray((1, 1))
 
-    for batch in range(10):
+    # CV image batch collectors, first element ignored later
+    cv_predictions = np.ndarray((1, 1))
+    cv_y = np.ndarray((1, 1))
+
+    for batch in range(100):
         print('CV batch', batch)
-        test_batch_a, test_batch_b, test_batch_y = get_batch(cv_pairs, (batch * BATCH_SIZE) % len(pairs), image_size)
-        predictions_batch = model.predict_on_batch([test_batch_a, test_batch_b])
+        cv_batch_a, cv_batch_b, cv_batch_y = get_batch(cv_pairs, (batch * BATCH_SIZE), image_size)
+        predictions_batch = model.predict_on_batch([cv_batch_a, cv_batch_b])
 
-        all_predictions = np.vstack((all_predictions, np.round(predictions_batch)))
-        all_y = np.vstack((all_y, test_batch_y.reshape(BATCH_SIZE, 1)))
+        cv_predictions = np.vstack((cv_predictions, np.round(predictions_batch)))
+        cv_y = np.vstack((cv_y, cv_batch_y.reshape(BATCH_SIZE, 1)))
 
 
-    siamese_utils.eval_siamese(all_y[1:], all_predictions[1:])
+    siamese_utils.eval_siamese(cv_y[1:], cv_predictions[1:])
 
 
 def get_batch(pairs, batch_id, image_size):
@@ -155,7 +168,7 @@ def load_trained_model(pretrained_model_file):
     return load_model(pretrained_model_file, custom_objects={'contrastive_loss': siamese_utils.contrastive_loss})
 
 # TODO:
-def competition_test_model(model, pairs_csv):
+def generate_predictions(model, pairs_csv):
     # Open the pre-made file of pairs
     with open(pairs_csv) as pairs_file:
         reader = csv.reader(pairs_file)
@@ -171,15 +184,13 @@ def competition_test_model(model, pairs_csv):
 
     for batch in range(2):
         batch_index, batch_a, batch_b = get_competition_batch(pairs, batch)
-        
-
         predictions_batch = model.predict_on_batch([batch_a, batch_b])
-
-        print(predictions_batch)
-
         for prediction in range(len(predictions_batch)):
             out = '%d' % batch_index[prediction] + ',' + str(predictions_batch[prediction]) + '\n'
             output.write(out)
+
+
+    # leftovers...
 
     output.close()
 
@@ -196,8 +207,8 @@ def get_competition_batch(pairs, batch_id):
         #
         # Configure this to fit your file structure...
         #
-        batch_a[j] = get_image(os.path.join('data', 'small', 'test', pairs[batch_id * BATCH_SIZE + j][1]))
-        batch_b[j] = get_image(os.path.join('data', 'small', 'test', pairs[batch_id * BATCH_SIZE + j][2]))
+        batch_a[j] = get_image(os.path.join('data', 'test', pairs[batch_id * BATCH_SIZE + j][1]))
+        batch_b[j] = get_image(os.path.join('data', 'test', pairs[batch_id * BATCH_SIZE + j][2]))
 
         # error will crash this...
 
@@ -210,22 +221,22 @@ def main():
     if len(sys.argv) < 2:
         model = create_model((IMAGE_DIM, IMAGE_DIM, 3), 'large')
         print(model.summary())
-        train_model(model, (IMAGE_DIM, IMAGE_DIM, 3), 500, os.path.join('data', 'pairwise_train_info.csv'))
+        train_model(model, (IMAGE_DIM, IMAGE_DIM, 3), 0.000005, os.path.join('data', 'pairwise_train_info.csv'))
 
     # Pre-trained model provided:
     else:
         # If the passed in file is a whole model, not just weights:
         # if bool(sys.argv[2]):
-        #     # model = load_trained_model(sys.argv[1])
+        model = load_trained_model(os.path.join('models','painting_model_model_1512870158.h5'))
         #     print('this filetype is not ready yet')
         # # Just have the weights:
         # else:
-        model = create_model((IMAGE_DIM, IMAGE_DIM, 3), 'large')
-        model.load_weights('painting_weights.h5')
+        # model = create_model((IMAGE_DIM, IMAGE_DIM, 3), 'large')
+        # model.load_weights(os.path.join('models','painting_model_model_1512870158.h5'))
 
 
 
-        competition_test_model(model, os.path.join('data', 'submission_info.csv'))
+        generate_predictions(model, os.path.join('data', 'submission_info.csv'))
 #         assumes: submission info in data
 #         TODO: run on testing set
 
